48748fe1ade32fa711030ed3f2d319c717e73e22
phfa416 <phf160217@qq.com>
2025-10-07 17:01:08 +0800

    Site updated: 2025-10-07 17:01:07

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759827374846";const preCache = ["/images/taichi.png","/images/914067.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759827664970";const preCache = ["/images/taichi.png","/images/914067.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
